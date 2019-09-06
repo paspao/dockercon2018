5bc796210d1250fa60315dd7667c51ee1f6acf34
Pasquale Paola <pasquale.paola@eng.it>
2019-09-06 10:59:24 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DockerCon 2018
+
+A presentation built with reveal.js of my experience at DockerCon 2018 Barcelona
---
+++ dockerExample/script.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+git clone https://paspaola:_Petillo25@production.eng.it/gitlab/FCA_OP_PILOTS/digital-fastening.git
